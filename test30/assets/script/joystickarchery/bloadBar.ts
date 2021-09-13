3dc10741f631e20ce153d13c5c62a17a400bf8b6
许三多 <sxu86227@gmail.com>
2021-09-13 13:55:53 +0700

    refresh  bloodbar  finish

--- test30/assets/script/joystickarchery/bloadBar.ts
+++ test30/assets/script/joystickarchery/bloadBar.ts
@@ -1,7 +1,8 @@
 
-import { _decorator, Component, Node, LayoutComponent, Prefab, UITransformComponent, Vec3, find } from 'cc';
+import { _decorator, Component, Node, LayoutComponent, Prefab, UITransformComponent, Vec3, find, UITransform, size, Size, tween } from 'cc';
 import { clientEvent } from '../framwork/clientEvent';
 import { Constant } from '../framwork/constant';
+import { PoolManager } from '../infinitymap/poolManager';
 import { ArcheryCamera } from './archeryCamera';
 const { ccclass, property } = _decorator;
 
@@ -34,10 +35,13 @@ export class BloadBar extends Component {
 
     private _followRole: Node = null!//跟随对象
     private _totalWight: number = 102;//血条长度(宽度)
-    private _totalBlood: number = 500;//总的血量
+    private _totalBlood: number = 5000;//总的血量
+    private _curBlood: number = 5000;//当前血量
+    private _ratio: number = 0;//血条长度与血量的比率
     private _maxItemBlood: number = 200;//每隔血条
     private _minBloodBarItemWidth: number = 10;//最小单个血块宽度
     private _bloodBarWidth: number = 0;//当前整体血条宽度
+    private _bloodBarHeight: number = 17;//血条高度
     private _curPos: Vec3 = new Vec3()!;//当前血条位置
     onLoad() {
         clientEvent.on(Constant.EVENT_TYPE.CarmeraRole, this._setFloowRole, this);
@@ -49,8 +53,12 @@ export class BloadBar extends Component {
 
     start() {
         //UI层级
-        this.node.setSiblingIndex(0);
-
+        this.node.setSiblingIndex(10);
+        this.show();
+        this.schedule(() => {
+            this._curBlood -= 1000;
+            this.show();
+        }, 2);
     }
 
     _setFloowRole(role: Node) {
@@ -58,11 +66,58 @@ export class BloadBar extends Component {
     }
 
     public show() {
+        this._refreshCurBlood();
+    }
+
+    /**
+     * 刷新血条进度
+     */
+    private _refreshCurBlood() {
+        this._curBlood = this._curBlood > 0 ? this._curBlood : this._totalBlood;
         let totalBlood = this._totalBlood;
-        //血块数量
+        //血条长度与血量的比率
+        this._ratio = this._totalWight / totalBlood;
+
+        /* ------------- 展示分隔线 ------------- */
+        //血块数量  line数量
         let bloodItemNum = Math.ceil(totalBlood / this._maxItemBlood);
-        //当前血量条最小长度
-        this._bloodBarWidth = this._minBloodBarItemWidth * bloodItemNum;
+        //layout X 偏移 计算每个分割线x位置
+        let layoutOffset = totalBlood / bloodItemNum * this._ratio;
+        for (let i = 0; i < bloodItemNum; i++) {
+            let line = this.layoutContainer.node.children[i];
+            if (!line) {
+                line = PoolManager.getNode(this.pbLine);
+            }
+            line.setPosition(new Vec3(layoutOffset * i, 0, 0));
+            line.active = false;
+            line.parent = this.layoutContainer.node;
+            line.getComponent(UITransform)?.setContentSize(1, 10);
+            if (i % 5 == 0) {
+                line.getComponent(UITransform)?.setContentSize(1, 15);
+            }
+        }
+
+        /* ------------- 展示当前血量 ------------- */
+        let curBlood = this._curBlood;
+        let bloodRadi = curBlood / this._totalBlood;
+        this._bloodBarWidth = bloodRadi * this._totalWight;
+        bloodItemNum = Math.ceil(curBlood / this._maxItemBlood);
+        tween(this.whiteBar).to(0.5, { contentSize: new Size(this._bloodBarWidth, this._bloodBarHeight) }).start();
+        tween(this.curBloodBar).to(0.2, { contentSize: new Size(this._bloodBarWidth, this._bloodBarHeight) }).start();
+
+        /* 只展示当前血量的分割线 */
+        for (let i = 0; i < bloodItemNum; i++) {
+            let line = this.layoutContainer.node.children[i];
+            if (!line) {
+                line = PoolManager.getNode(this.pbLine);
+            }
+            line.setPosition(new Vec3(layoutOffset * i, 0, 0));
+            line.active = true;
+            line.parent = this.layoutContainer.node;
+            if (i == 0) {//第一个不用显示
+                line.active = false;
+            }
+        }
     }
 
     update(deltaTime: number) {
